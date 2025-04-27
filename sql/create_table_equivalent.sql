45fc2ff27ec1aafc3c881b0bfbe97f630210e902
SamBroomy <36888606+SamBroomy@users.noreply.github.com>
2025-04-27 18:40:26 +0100

    multi level search

--- sql/create_table_equivalent.sql
+++ sql/create_table_equivalent.sql
@@ -12,7 +12,7 @@ FROM (
         WHERE feature_class = 'P'
     ) AS P ON COALESCE(A.name, 'N/A') = COALESCE(P.name, 'N/A')
     AND COALESCE(A.asciiname, 'N/A') = COALESCE(P.asciiname, 'N/A')
-    AND COALESCE(A.ISO, 'N/A') = COALESCE(P.ISO, 'N/A')
+    AND COALESCE(A.admin0_code, 'N/A') = COALESCE(P.admin0_code, 'N/A')
     AND COALESCE(A.admin1_code, 'N/A') = COALESCE(P.admin1_code, 'N/A')
     AND COALESCE(A.admin2_code, 'N/A') = COALESCE(P.admin2_code, 'N/A')
     AND COALESCE(A.admin3_code, 'N/A') = COALESCE(P.admin3_code, 'N/A')
